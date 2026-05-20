82b87a4eaf2ac6158fe6d0bb18a8d1e237caafc2
Meghanashree Manjunath <shree.meghana182000@gmail.com>
2026-05-20 17:48:35 +0530

    ASSIGNMENT3

---
+++ Program1.java
@@ -0,0 +1,22 @@
+package weekly_assignment1;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float marks = 87.6f;
+		double value1 = marks;
+		int value2 = (int)value1;
+		{
+			System.out.println("FLOAT VALUE :" + marks);
+			System.out.println("DOUBLE VALUE :" + value1);
+			System.out.println("INTEGER VALUE :" + value2);
+		}
+		char ascii = (char)value2;
+		{
+			System.out.println("ASCII VALUE :" + ascii);
+		}
+
+	}
+
+}
