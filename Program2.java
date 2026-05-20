f3e810daf91252a0434189f72f7329f8273f8e2b
Meghanashree Manjunath <shree.meghana182000@gmail.com>
2026-05-20 17:50:11 +0530

    ASSIGNMENT4

---
+++ Program2.java
@@ -0,0 +1,20 @@
+package weekly_assignment1;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char value1 = 'A';
+		int value2 = value1;
+		int value3 = 5 + value2;
+		char value4 = (char)value3;
+		{
+			System.out.println("CHARATER VALUE :" + value1);
+			System.out.println("INTEGER VALUE :" + value2);
+			System.out.println("MULTIPLED VALUE :" + value3);
+			System.out.println("ASCII CHARATER :" + value4);
+		}
+
+	}
+
+}
